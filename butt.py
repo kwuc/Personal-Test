783f520bd4e5ee2d44e5a8d927d5939735d1044e
kwuc <61835807+kwuc@users.noreply.github.com>
2020-05-20 16:15:57 -0400

    Hello World

---
+++ butt.py
@@ -0,0 +1,3 @@
+# Butt.py
+poop = input("How many time did you poop today?\n")
+print ("You pooped ",poop," times today")
---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#Madlibs.py
+
+adjective = input ("Please enter an adjetive: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending in -ed: ")
+print("Your Madlib:")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
+
+## Today we are announcing the biggest <something> ever!
+
+something = input("A silly person place or thing:\n")
+print ("Today we are announcing the biggest",something,"ever!")
